51465a0377c1431dd285f13f0b01fdb07e347707
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2023-08-05 16:30:25 -0600

    Add functionality to support instances of no results

--- app/facades/recipes_facade.rb
+++ app/facades/recipes_facade.rb
@@ -1,7 +1,14 @@
 class RecipesFacade
   def recipes_index(search)
-    details = service.recipes_by_country(search)
-    @recipes = details[:hits].map { |recipe| Recipe.new(recipe[:recipe], search) }
+    if search == ''
+      countries = CountriesFacade.new.countries_index
+      search = countries.sample.name
+      details = service.recipes_by_country(search)
+      @recipes = if details[:hits].empty? then [] else details[:hits].map { |recipe| Recipe.new(recipe[:recipe], search) } end
+    else
+      details = service.recipes_by_country(search)
+      @recipes = if details[:hits].empty? then [] else details[:hits].map { |recipe| Recipe.new(recipe[:recipe], search) } end
+    end
   end
 
   def service
